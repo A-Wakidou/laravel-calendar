2d763a6741aa1da564d0c4ebdd83202cb026b47b
A-Wakidou <57793874+A-Wakidou@users.noreply.github.com>
2022-01-18 22:56:36 +0100

    adding crud events for google calendar

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 use App\Models\Events;
+use Spatie\GoogleCalendar\Event;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class CalendarController extends Controller
 {
@@ -13,7 +15,17 @@ class CalendarController extends Controller
     }
 
     public function createEvent(Request $request) {
+
+        //Google calendar
+        $event = new Event;
+        $event->name = $request->input('title');
+        $event->startDateTime = Carbon::parse($request->input('startDate'));
+        $event->endDateTime = Carbon::parse($request->input('endDate'));
+        $newEvent = $event->save();
+
+        //Database
         $query = DB::table('events')->insert([
+            'google_calendar_event_id'=> $newEvent->id,
             'title'=>$request->input('title'),
             'start'=>$request->input('startDate'),
             'end'=>$request->input('endDate'),
@@ -27,17 +39,32 @@ class CalendarController extends Controller
     }
 
     public function editEvent(Request $request) {
+
+        //Database
         $editItem = Events::find($request->id);
         $editItem->title = $request->title;
         $editItem->start = $request->startDate;
         $editItem->end = $request->endDate;
         $editItem->save();
+
+        //Google calendar
+        $event = Event::find($editItem->google_calendar_event_id);
+        $event->name = $editItem->title;
+        $event->startDateTime = Carbon::parse($editItem->start);
+        $event->endDateTime = Carbon::parse($editItem->end);
+        $event->save();
         return back();
     }
 
     public function deleteEvent(Request $request) {
+
+        //Database
         $deleteItem = Events::find($request->id);
         $deleteItem->delete();
+
+        //Google calendar
+        $event = Event::find($deleteItem->google_calendar_event_id);
+        $event->delete();
         return back();
     }
 }
